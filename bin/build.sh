9556c84ee8f5622b24631aa058f6e2244e82000c
James Collings <james@jclabs.co.uk>
2025-05-13 22:08:31 +0100

    Change readme version number

--- bin/build.sh
+++ bin/build.sh
@@ -32,6 +32,7 @@ git rm -rf .
 rsync -av .. . --exclude '.git' --exclude '.github' --exclude 'bin' --exclude "$FOLDER" --exclude 'node_modules' --exclude 'src' --exclude 'tests' --exclude 'vendor' --exclude '.babelrc' --exclude '.gitattributes' --exclude '.gitignore' --exclude '.phpcs.xml.dist' --exclude '.phpunit.result.cache' --exclude '.travis.yml' --exclude 'composer.json' --exclude 'composer.lock' --exclude 'package-lock.json' --exclude 'package.json' --exclude 'phpunit.xml.dist' --exclude 'webpack.config.js' --exclude '.wp-env.json'
 
 # Set version numbers
+sed -i -r "s/^\*\*Version: (.+)\*\*$/\*\*Version: $TAG\*\*/g" README.md
 sed -i -r "s/^ \* Version: (.+) $/ \* Version: $TAG /g" importwp-wpml.php
 
 # Confirm pushing of build.
